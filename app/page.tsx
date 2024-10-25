9636797844aa55cd71249224e35c59acf107e42c
dultengmax <aziznasirudin@gmail.com>
2024-10-25 16:38:20 +0700

    hari kedua

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import { ThreeDCardDemo } from "@/components/assets/card";
-import { NavbarDemo } from "@/components/assets/navbar";
+import { CardHoverEffectDemo } from "@/components/assets/card_hover";
+import { NavbarDemo, NavigationMenuDemo } from "@/components/assets/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
 <>
-<div>
-  asdsad
+<div className="flex justify-center z-20 bg-white shadow-md">
+<NavigationMenuDemo/>
 </div>
-<NavbarDemo/>
+{/* <NavbarDemo/> */}
+<div className="flex justify-center">
+  <CardHoverEffectDemo/>
 <ThreeDCardDemo />
-<i className="fi fi-br-link-alt"></i>
+</div>
 </>
   );
 }
